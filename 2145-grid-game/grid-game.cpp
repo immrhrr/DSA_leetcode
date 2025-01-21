6b6b3a54e797a5d91381eb89149ad78b73928614
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-01-21 22:16:49 +0530

    Time: 4 ms (47.51%) | Memory: 66.7 MB (76.92%) - LeetSync

--- 2145-grid-game/grid-game.cpp
+++ 2145-grid-game/grid-game.cpp
@@ -7,14 +7,9 @@ class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
         int n = grid[0].size();
-        vector<long long> a(n, 0), b(n, 0);
-        
-        // Prefix sums for the top and bottom rows
-        a[0] = grid[0][0];
-        b[0] = grid[1][0];
-        for (int i = 1; i < n; i++) {
-            a[i] = a[i - 1] + grid[0][i];
-            b[i] = b[i - 1] + grid[1][i];
+        long long top=0;
+        for (int i = 0; i < n; i++) {
+           top+=grid[0][i];
         }
         
         long long ans = LLONG_MAX;
@@ -22,7 +17,7 @@ public:
         
         for (int i = 0; i < n; i++) {
             // Maximum points the opponent can collect after choosing column i
-            long long top = a[n - 1] - a[i];
+           top=top-grid[0][i];
             long long bottom = temp;
             ans = min(ans, max(top, bottom));
             temp += grid[1][i];
