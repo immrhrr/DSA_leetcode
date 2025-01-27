cad5dec9cf67c5365d0105ce0dc5961a3fe9ea6d
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-01-28 03:19:12 +0530

    Time: 895 ms (5.00%) | Memory: 70.1 MB (60.14%) - LeetSync

---
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public: 
+
+    bool dfs(unordered_map<int,vector<int>>&adj,int curr,int &dest,vector<bool>&visited){
+        visited[curr]=true;
+        if(dest==curr){
+            return true;
+        }
+        bool flag=false;
+        for(int i=0;i<adj[curr].size();i++){
+            int temp=adj[curr][i];
+             if(dest==temp){
+            return true;
+        }
+            if(visited[temp]==false){
+                flag=flag||dfs(adj,temp,dest,visited);
+                visited[curr]=true;
+            }
+
+        }
+        return flag;
+
+    }
+    vector<bool> checkIfPrerequisite(int num, vector<vector<int>>& pre, vector<vector<int>>& q) {
+        int m=q.size();
+        vector<bool>ans(m,false);
+        int n=pre.size();
+        unordered_map<int,vector<int>>adj;
+        for(int i=0;i<n;i++){
+            int u=pre[i][0];
+            int v=pre[i][1];
+            adj[u].push_back(v);
+        }
+        for(int i=0;i<m;i++){
+            int curr=q[i][0];
+            int dest=q[i][1];
+            vector<bool>visited(num,false);
+            if(dfs(adj,curr,dest,visited)){
+                ans[i]=true;
+            }
+        }
+        return ans;
+
+    }
+};
