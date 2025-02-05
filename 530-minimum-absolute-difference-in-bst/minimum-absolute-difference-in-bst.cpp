9b36c92d6cf4e114a0fa5131b79ba3d8bfe26b2d
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-02-05 07:54:29 +0530

    Time: 1 ms (20.36%) | Memory: 25.5 MB (69.62%) - LeetSync

---
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void func(TreeNode*root,int&prev,int&ans){
+        if(root==NULL){
+            return;
+        }
+        //inorder
+        func(root->left,prev,ans);
+        if(prev!=INT_MIN){
+            ans=min(ans,abs(root->val-prev));
+        }
+        prev=root->val;
+        func(root->right,prev,ans);
+    }
+    int getMinimumDifference(TreeNode* root) {
+        int prev=INT_MIN;
+        int ans=INT_MAX;
+        func(root,prev,ans);
+        return ans;
+    }
+};
