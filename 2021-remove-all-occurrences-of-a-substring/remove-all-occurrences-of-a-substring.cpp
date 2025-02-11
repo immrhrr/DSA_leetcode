b3abee3118212d51e5e169c2261d80ed9659767f
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-02-11 18:39:17 +0530

    Time: 15 ms (5.85%) | Memory: 12 MB (9.01%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        int n=s.size();
+        int m=part.size();
+      //  reverse(part.begin(),part.end());
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            st.push(s[i]);
+            if(st.top()==part[m-1]){
+                string temp;
+                for(int j=m-1;j>=0;j--){
+                    if(!st.empty()&&st.top()==part[j]){
+                         temp+=st.top();
+                         st.pop();
+                    }
+                    else{
+                        break;
+                    }
+                }
+                reverse(temp.begin(),temp.end());
+                if(temp!=part){
+                    int x=temp.size();
+                    for(int k=0;k<x;k++){
+                        st.push(temp[k]);
+                    }
+                }
+            }
+        }
+        string ans;
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
