b604e2a71dc2f7786b8299884d4ffff75953cb6a
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-02-12 14:31:39 +0530

    Time: 5 ms (96.25%) | Memory: 64 MB (52.02%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -12,20 +12,44 @@ public:
     }
     int maximumSum(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>temp(82);
+        vector<vector<int>>temp(82,vector<int>(2,-1));
         for(int i=0;i<n;i++){
             int digit_sum=func(nums[i]);
-            temp[digit_sum].push_back(nums[i]);
-        }
-        int ans=-1;
-        for(int i=0;i<82;i++){
-            int size=temp[i].size();
-            if(size<=1){
+           if(temp[digit_sum][1]==-1){
+            temp[digit_sum][1]=nums[i];
+           }
+           else if(temp[digit_sum][0]==-1){
+            temp[digit_sum][0]=nums[i];
+           }
+           else{
+            int smaller=min(temp[digit_sum][0],temp[digit_sum][1]);
+            int bigger=max(temp[digit_sum][0],temp[digit_sum][1]);
+            //no need to update
+            if(nums[i]<=smaller){
                 continue;
             }
-            sort(temp[i].begin(),temp[i].end());
-            ans=max(ans,temp[i][size-2]+temp[i][size-1]);
+            //need to update
+            //smaller of these two will be replaced
+            else{
+                //first number is bigger than second
+                //second will be replaced
+                if(temp[digit_sum][0]>temp[digit_sum][1])
+                temp[digit_sum][1]=nums[i];
+                //second is bigger than first
+                //first will be replaced
+                else
+                temp[digit_sum][0]=nums[i];
+            }
         }
+    }
+        int ans=-1;
+        for(int i=0;i<82;i++){
+            if(temp[i][0]==-1||temp[i][1]==-1){
+                continue;
+            }
+            ans=max(ans,temp[i][0]+temp[i][1]);
+        }
+       
         return ans;
     }
 };
