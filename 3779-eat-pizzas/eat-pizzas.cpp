68b9de4814bcb5fa81d48146c5c520701d081830
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-02-16 18:26:24 +0530

    Time: 74 ms (66.67%) | Memory: 180 MB (66.67%) - LeetSync

--- 3779-eat-pizzas/eat-pizzas.cpp
+++ 3779-eat-pizzas/eat-pizzas.cpp
@@ -3,18 +3,24 @@ public:
     long long maxWeight(vector<int>& pizzas) {
         long long ans=0;
         int n=pizzas.size();
-       priority_queue<int>pq(pizzas.begin(),pizzas.end());
+      // priority_queue<int>pq(pizzas.begin(),pizzas.end());
+       sort(pizzas.begin(),pizzas.end());
+       int idx=n-1;
         int days=n/4;
         int odd_days=(days+1)/2;
         int even_days=days-odd_days;
         for(int i=0;i<odd_days;i++){
-            ans+=pq.top();
-            pq.pop();
+            ans+=pizzas[idx];
+            idx--;
+            // ans+=pq.top();
+            // pq.pop();
         }
         for(int i=0;i<even_days;i++){
-            pq.pop();
-            ans+=pq.top();
-            pq.pop();
+            idx--;
+            ans+=pizzas[idx--];
+            // pq.pop();
+            // ans+=pq.top();
+            // pq.pop();
         }
         return ans;
     }
