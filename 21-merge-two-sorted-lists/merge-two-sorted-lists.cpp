bb91de551715b83d29ae310037abe8419db416c2
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-02-22 23:20:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (28.46%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,33 +11,38 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* t1 = list1;
-        ListNode* t2 = list2;
+        if(list1==NULL){
+            return list2;
+        }
+         if(list2==NULL){
+            return list1;
+        }
         // creating dummy node
         ListNode* head = new ListNode(0);
         ListNode* tail = head;
-        while (t1 && t2) {
-            // t1 ka data is bigger than t2 ka data
-            if (t1->val > t2->val) {
-                tail->next = t2;
+        while (list1 && list2) {
+            // list1 ka data is bigger than t2 ka data
+            if (list1->val > list2->val) {
+                tail->next = list2;
                 tail = tail->next;
-               
-                t2 = t2->next;
+                list2 = list2->next;
             }
-            // t2 ka data is bigger than or equal to t1 ka data
+            // list2 ka data is bigger than or equal to t1 ka data
             else {
-                tail->next = t1;
+                tail->next = list1;
                 tail = tail->next;
-               
-                t1 = t1->next;
+                list1 = list1->next;
             }
         }
-        if(t1){
-            tail->next = t1;
+        //lest1 has nodes left
+        if(list1){
+            tail->next = list1;
         }
-         if(t2){
-            tail->next = t2;
+        //list2 has nodes left
+         if(list2){
+            tail->next = list2;
         }
+        //remove dummy variables
         head = head->next;
         return head;
     }
