849bc261f65ab63d2a0c9c753271ce9d9138ea8a
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-03-07 03:48:43 +0530

    Time: 2 ms (72.65%) | Memory: 20.3 MB (14.51%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    int solve(vector<int>&frq,int ind,vector<int>&dp){
-        int n=frq.size();
-        if(ind>=n){
-            return 0;
-        }
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-        int take=0;
-        int notake=0;
-        take=frq[ind]+solve(frq,ind+2,dp);
-        notake=solve(frq,ind+1,dp);
-        dp[ind]=max(take,notake);
-        return dp[ind];
-    }
     int deleteAndEarn(vector<int>& nums) {
         int n=nums.size();
         vector<int>frq(1e4+1,0);
-        vector<int>dp(1e4+1,-1);
+        vector<int>dp(1e4+1,0);
         for(int i=0;i<n;i++){
             frq[nums[i]]+=nums[i];
         }
-        return solve(frq,0,dp);
+        dp[1]=frq[1];
+        for(int i=2;i<frq.size();i++){
+           
+            dp[i]=max(dp[i-1],dp[i-2]+frq[i]);
+            
+        }
+        return dp[frq.size()-1];
     }
 };
