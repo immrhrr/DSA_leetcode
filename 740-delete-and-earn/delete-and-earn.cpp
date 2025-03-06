06bb388fb582121b0e95eaf12413cd9f8185132f
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-03-07 03:50:15 +0530

    Time: 7 ms (40.22%) | Memory: 20.3 MB (14.51%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -7,11 +7,11 @@ public:
         for(int i=0;i<n;i++){
             frq[nums[i]]+=nums[i];
         }
+        // int prev=0;
+        // int curr=0;
         dp[1]=frq[1];
         for(int i=2;i<frq.size();i++){
-           
             dp[i]=max(dp[i-1],dp[i-2]+frq[i]);
-            
         }
         return dp[frq.size()-1];
     }
