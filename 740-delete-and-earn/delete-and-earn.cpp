546eac4989a3fbbb4ea6586d0a33102fb1d98d81
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-03-07 03:55:50 +0530

    Time: 2 ms (72.65%) | Memory: 17.7 MB (41.70%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -3,16 +3,18 @@ public:
     int deleteAndEarn(vector<int>& nums) {
         int n=nums.size();
         vector<int>frq(1e4+1,0);
-        vector<int>dp(1e4+1,0);
         for(int i=0;i<n;i++){
             frq[nums[i]]+=nums[i];
         }
-        // int prev=0;
-        // int curr=0;
-        dp[1]=frq[1];
+        int prev1=frq[1];
+        int prev2=0;
+        int curr=prev1;
         for(int i=2;i<frq.size();i++){
-            dp[i]=max(dp[i-1],dp[i-2]+frq[i]);
+        
+           curr=max(prev1,prev2+frq[i]);
+           prev2=prev1;
+           prev1=curr;
         }
-        return dp[frq.size()-1];
+        return curr;
     }
 };
