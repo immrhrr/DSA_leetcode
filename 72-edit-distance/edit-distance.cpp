c1f9b4af361f212702d753ea6eb35426427f318b
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-03-10 19:55:18 +0530

    Time: 8 ms (45.90%) | Memory: 13.4 MB (25.31%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minDistance(string w1, string w2) {
+        int n1=w1.size();
+        int n2=w2.size();
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+        for(int i=0;i<=n1;i++){
+            dp[i][0]=i;
+        }
+        for(int i=0;i<=n2;i++){
+            dp[0][i]=i;
+        }
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+              
+                if(w1[i-1]==w2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+};
