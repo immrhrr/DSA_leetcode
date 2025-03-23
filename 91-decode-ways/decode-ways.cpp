d1f8d46548383ac71de1d6e31a3cfe02448829b6
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-03-23 07:19:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (69.94%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -25,22 +25,21 @@ public:
     int numDecodings(string digits) {
         int n=digits.size();
         //first digit can not be zero
-       if(digits[0]=='0'){
-           return 0;
-       }
+    //    if(digits[0]=='0'){
+    //        return 0;
+    //    }
        vector<int>dp(n+1,-1);
-      
-       //if there is number other than 1,2 before any zero then zero answer
-       //there should not be two consecutive zero also
-       for(int i=1;i<n;i++){
-           if(digits[i]=='0'){
-              int prev=digits[i-1]-'0';
-              if(prev>2||prev==0){
-                  return 0;
-              }
-           }
+    //    //if there is number other than 1,2 before any zero then zero answer
+    //    //there should not be two consecutive zero also
+    //    for(int i=1;i<n;i++){
+    //        if(digits[i]=='0'){
+    //           int prev=digits[i-1]-'0';
+    //           if(prev>2||prev==0){
+    //               return 0;
+    //           }
+    //        }
            
-       }
+    //    }
         return solve(digits,0,dp);
         
     }
