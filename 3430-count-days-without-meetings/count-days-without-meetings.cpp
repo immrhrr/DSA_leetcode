9a38429a3fab924f35a7f9cc9e9e9e91c78bf201
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-03-24 21:03:38 +0530

    Time: 70 ms (55.78%) | Memory: 126.4 MB (94.73%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -3,9 +3,10 @@ public:
     int countDays(int days, vector<vector<int>>& meetings) {
         int n=meetings.size();
         sort(meetings.begin(),meetings.end());
-        vector<pair<int,int>>temp;
+        
         int prev_start=meetings[0][0];
         int prev_end=meetings[0][1];
+        int cnt=0;
         for(int i=1;i<n;i++){
             int curr_start=meetings[i][0];
             int curr_end=meetings[i][1];
@@ -13,17 +14,14 @@ public:
                 prev_end=max(prev_end,curr_end);
             }
             else{
-                temp.push_back({prev_start,prev_end});
+              
+                cnt+=(prev_end-prev_start+1);
                 prev_start=curr_start;
                 prev_end=curr_end;
             }
         }
-        temp.push_back({prev_start,prev_end});
-        int cnt=0;
-        for(int i=0;i<temp.size();i++){
-            cnt+=(temp[i].second-temp[i].first+1);
-
-        }
+      
+         cnt+=(prev_end-prev_start+1);
         return days-cnt;
     }
 };
