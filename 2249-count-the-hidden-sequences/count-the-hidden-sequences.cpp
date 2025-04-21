24aa18bb3da99a465cdd90ead1429cc1417306fe
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-04-21 23:21:49 +0530

    Time: 1 ms (47.58%) | Memory: 112.5 MB (75.00%) - LeetSync

--- 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int numberOfArrays(vector<int>& diff, int l, int u) {
-        int curr=1;
-        int mini=1;
-        int maxi=1;
+        int curr=100;
+        int mini=100;
+        int maxi=100;
         for(int i=0;i<diff.size();i++){
             curr=curr+diff[i];
             mini=min(mini,curr);
