721e17dfb10ad10348d822cd3bd58fd992c324a7
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-04-30 22:45:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (41.28%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i=0;
+        int n=nums.size();
+        //find the first occ of val
+        while(i<n&&nums[i]!=val){
+            i++;
+        }
+        if(i==n)return n;
+        int j=n-1;
+        //find the first index from back where its not equal to val
+        while(j>=0&&nums[j]==val){
+            j--;
+        }
+        if(j==-1)return 0;
+        while(i<j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+            while(nums[i]!=val){
+                i++;
+            }
+            while(nums[j]==val){
+                j--;
+            }
+           
+
+        }
+        
+        return i;
+        
+    }
+};
