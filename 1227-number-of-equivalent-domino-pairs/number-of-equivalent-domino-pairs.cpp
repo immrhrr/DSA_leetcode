9518b5f66c5d40523327a3e60b35965cfde16d26
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-05-04 19:02:01 +0530

    Time: 6 ms (43.75%) | Memory: 26.2 MB (74.00%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>>& dominoes) {
-        unordered_map<string,int>mp;
+        unordered_map<int,int>mp;
         int n=dominoes.size();
         for(int i=0;i<n;i++){
             int a=max(dominoes[i][0],dominoes[i][1]);
             int b=min(dominoes[i][0],dominoes[i][1]);
-            mp[to_string(a)+"and"+to_string(b)]++;
+            mp[10*a+b]++;
         }
         int ans=0;
         for(auto it:mp){
