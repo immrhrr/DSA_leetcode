fb599fdd980e599fbf84583e1f3258b51878cf63
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-05-22 22:23:31 +0530

    Time: 78 ms (30.90%) | Memory: 341.3 MB (47.48%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,43 +1,49 @@
 class Solution {
 public:
-
-
-    bool check(vector<int>nums,int k,vector<vector<int>>&q){
+    bool check(vector<int>&nums,vector<vector<int>>&q,int k){
         int n=nums.size();
-        vector<int>diff(n,0);
-        bool flag=true;
-        for(int i=0;i<k;i++){
+        vector<int>temp(n,0);
+        for(int i=0;i<=k;i++){
             int l=q[i][0];
-            int r=q[i][1];
+            int r=q[i][1]+1;
             int val=q[i][2];
-            diff[l]+=val;
-            if(r+1<n)
-            diff[r+1]+=-val;
-        }
-        int cumsum=0;
-        for(int i=0;i<n;i++){
-            cumsum+=diff[i];
-            if(nums[i]>cumsum){
-                return false;
+            if(r<n){
+                temp[r]+=(-val);
             }
+            temp[l]+=val;
+        }
+        for(int i=1;i<n;i++){
+            temp[i]=temp[i]+temp[i-1];
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>temp[i])return false;
         }
         return true;
     }
     int minZeroArray(vector<int>& nums, vector<vector<int>>& q) {
+        //min k such that by processing first k quesrries it becomes zero
         int n=nums.size();
-       
+        bool flag=true;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                flag=false;
+                break;
+            }
+        }
+        if(flag)return 0;
+        int m=q.size();
         int low=0;
-        int high=q.size();
+        int high=m-1;
         int ans=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-           if(check(nums,mid,q)){
-            ans=mid;
-            high=mid-1;
-           }
-           else{
-            low=mid+1;
-           }
+            if(check(nums,q,mid)){
+                high=mid-1;
+                ans=mid+1;
+            }
+            else{
+                low=mid+1;
+            }
         }
         return ans;
         
