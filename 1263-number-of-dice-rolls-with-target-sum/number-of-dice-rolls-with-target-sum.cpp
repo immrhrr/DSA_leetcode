71fcd857d498d182a3b73dbb7bd97651d91bfbbf
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-05-23 19:48:00 +0530

    Time: 11 ms (83.51%) | Memory: 11.2 MB (50.89%) - LeetSync

---
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+     int mod=1e9+7;
+     int solve(int m,int n,int x,int sum,vector<vector<int>>&dp){
+        if(n==0){
+            return sum==x ? 1 : 0;
+        }
+        if(dp[n][sum]!=-1)return dp[n][sum];
+        int ways=0;
+        for(int i=1;i<=m;i++){
+            if(sum+i<=x){
+                ways=(ways+(solve(m,n-1,x,sum+i,dp))%mod)%mod;
+                
+            }
+        }
+        return dp[n][sum]= ways;
+    }
+    int numRollsToTarget(int n, int k, int target) {
+         vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
+        return solve(k,n,target,0,dp);
+    }
+};
