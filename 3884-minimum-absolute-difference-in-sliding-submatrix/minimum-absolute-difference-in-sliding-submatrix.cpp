53ac2311bd64e8c0afee8705e08a8ce0d0323fea
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-06-01 12:55:16 +0530

    Time: 95 ms (5.55%) | Memory: 63 MB (5.55%) - LeetSync

--- 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.cpp
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
-   int solve(unordered_set<int>st){
-    vector<int>num;
-    for(auto it:st){
-        num.push_back(it);
-    }
-    int ans=INT_MAX;
-    for(int i=0;i<num.size();i++){
-        for(int j=i+1;j<num.size();j++){
-            ans=min(ans,abs(num[i]-num[j]));
+    int solve(unordered_set<int> st) {
+        vector<int> num;
+        for (auto it : st) {
+            num.push_back(it);
         }
+        // int ans=INT_MAX;
+        // for(int i=0;i<num.size();i++){
+        //     for(int j=i+1;j<num.size();j++){
+        //         ans=min(ans,abs(num[i]-num[j]));
+        //     }
+        // }
+        // return ans;
+        sort(num.begin(), num.end());
+        int ans = INT_MAX;
+        for (int i = 1; i < num.size(); ++i)
+            ans = min(ans, num[i] - num[i - 1]);
+        return ans;
     }
-    return ans;
-   }
     vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>>ans(m-k+1,vector<int>(n-k+1,0));
-        for(int i=0;i<m-k+1;i++){
-            for(int j=0;j<n-k+1;j++){
-                unordered_set<int>st;
-                for(int l=i;l<i+k;l++){
-                    for(int m=j;m<j+k;m++){
-                       st.insert(grid[l][m]);
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> ans(m - k + 1, vector<int>(n - k + 1, 0));
+        for (int i = 0; i < m - k + 1; i++) {
+            for (int j = 0; j < n - k + 1; j++) {
+                unordered_set<int> st;
+                for (int l = i; l < i + k; l++) {
+                    for (int m = j; m < j + k; m++) {
+                        st.insert(grid[l][m]);
                     }
                 }
-                if(st.size()==1){
+                if (st.size() == 1) {
                     continue;
                 }
-                ans[i][j]=solve(st);
+                ans[i][j] = solve(st);
             }
         }
         return ans;
