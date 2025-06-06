466fb7a2c193f61005be4ccff6698a678d09e8f9
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-06-07 01:58:27 +0530

    Time: 53 ms (80.94%) | Memory: 19.6 MB (92.46%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int ladderLength(string start, string end, vector<string>& bank) {
+        if(start==end)return 0;
+        int n=bank.size();
+        int ans=1;
+        unordered_set<string>st(bank.begin(),bank.end());
+        if(st.find(end)==st.end())return 0;
+        queue<string>q;
+        vector<char>vec={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        q.push(start);
+        while(!q.empty()){
+            int m=q.size();
+            ans++;
+            while(m--){
+                string curr=q.front();
+                q.pop();
+                for(int i=0;i<end.size();i++){
+                    string temp=curr;
+                    for(int j=0;j<26;j++){
+                        temp[i]=vec[j];
+                        if(st.find(temp)!=st.end()){
+                            if(temp==end)return ans;
+                            q.push(temp);
+                            st.erase(temp);
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+        
+    }
+};
