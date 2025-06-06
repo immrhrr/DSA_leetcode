1a889a4385331bc60398f8dc42a8f07730febbe5
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-06-06 17:23:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (58.67%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     int minMutation(string start, string end, vector<string>& bank) {
-        unordered_set<string> st(bank.begin(), bank.end());
-        if (!st.count(end)) return -1;
-
-        queue<string> q;
+        if(start==end)return 0;
+        int n=bank.size();
+        int ans=0;
+        unordered_set<string>st;
+        for(int i=0;i<n;i++){
+            st.insert(bank[i]);
+        }
+        queue<string>q;
+        vector<char>vec={'A','T','G','C'};
         q.push(start);
-        int level = 0;
-        unordered_set<string> visited;
-        visited.insert(start);
-        vector<char> genes = {'A', 'C', 'G', 'T'};
-
-        while (!q.empty()) {
-            int size = q.size();
-            while (size--) {
-                string curr = q.front();
+        while(!q.empty()){
+            int m=q.size();
+            ans++;
+            while(m--){
+                string curr=q.front();
                 q.pop();
-                if (curr == end) return level;
-
-                for (int i = 0; i < curr.size(); ++i) {
-                    char original = curr[i];
-                    for (char c : genes) {
-                        curr[i] = c;
-                        if (st.count(curr) && !visited.count(curr)) {
-                            q.push(curr);
-                            visited.insert(curr);
+                for(int i=0;i<end.size();i++){
+                    string temp=curr;
+                    for(int j=0;j<4;j++){
+                        temp[i]=vec[j];
+                        if(st.find(temp)!=st.end()){
+                            if(temp==end)return ans;
+                            q.push(temp);
+                            st.erase(temp);
                         }
                     }
-                    curr[i] = original; // revert back
                 }
             }
-            level++;
         }
-
         return -1;
+        
     }
 };
