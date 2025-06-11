ac3ea7461639e237ba885b563ee21e85a86db45b
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-06-11 16:00:02 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (87.27%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -5,8 +5,8 @@ public:
         int curr=0;
         for(int i=0;i<n;i++){
             if(i>curr)return false;
-            if(curr>=n-1)return true;
             curr=max(curr,i+nums[i]);
+            if(curr>=n-1)return true;
         }
         return false;
         
