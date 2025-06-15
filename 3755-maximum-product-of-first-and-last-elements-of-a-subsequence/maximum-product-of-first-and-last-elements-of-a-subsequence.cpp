f42333608491e6230953bde880bd4a77594e2869
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-06-15 17:01:50 +0530

    Time: 216 ms (0.00%) | Memory: 217 MB (0.00%) - LeetSync

--- 3755-maximum-product-of-first-and-last-elements-of-a-subsequence/maximum-product-of-first-and-last-elements-of-a-subsequence.cpp
+++ 3755-maximum-product-of-first-and-last-elements-of-a-subsequence/maximum-product-of-first-and-last-elements-of-a-subsequence.cpp
@@ -4,26 +4,11 @@ public:
         int n=nums.size();
         priority_queue<pair<int,int>>maxi;
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>mini;
-        int cnt_pos=0;
-        int last_pos_ind=-1;
-        int first_pos_ind=-1;
-        bool flag=false;
         for(int i=m-1;i<n;i++){
            maxi.push({nums[i],i});
            mini.push({nums[i],i});
-           if(!flag&&nums[i]>=0){
-            first_pos_ind=i;
-            flag=true;
-           }
-           if(nums[i]>=0){
-            cnt_pos++;
-            last_pos_ind=i;
-           }
         }
         long long ans=-1*1e10;
-        // if(cnt_pos<2||(last_pos_ind-first_pos_ind+1<m)){
-        //     ans=-1;
-        // }
         for(int i=0;i<=n-m;i++){
            int j=maxi.top().second;
            while(!maxi.empty()&&j-i+1<m){
@@ -40,7 +25,6 @@ public:
            long long small=mini.top().first;
            ans=max({ans,nums[i]*big,nums[i]*small});
         }
-        
         return ans;
         
     }
