10aac2b7768908ab38d21e79446826b34ba71036
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-06-15 22:40:00 +0530

    Time: 90 ms (5.28%) | Memory: 40.7 MB (5.28%) - LeetSync

--- 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.cpp
+++ 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     bool check(string&s,int start,int end){
-        unordered_set<string>st;
-        st.insert("11110100001001");
-        st.insert("110000110101");
-        st.insert("1");
-        st.insert("101");
-        st.insert("1001110001");
-        st.insert("1111101");
-        st.insert("11001");
+         unordered_set<string> st = {
+            "1", "101", "11001", "1111101", "1001110001", 
+            "110000110101", "11110100001001"
+        };
         string temp=s.substr(start,end-start+1);
         if(st.find(temp)==st.end())return false;
         return true;
     }
-    int solve(string&s,int ind){
+    int solve(string&s,int ind,vector<int>&dp){
         int n=s.size();
         if(ind>=n)return 0;
+        if(dp[ind]!=-1)return dp[ind];
         int ans=100;
         int temp=0;
         for(int i=ind;i<n;i++){
             if(check(s,ind,i)){
-                int temp=solve(s,i+1);
+                int temp=solve(s,i+1,dp);
                 if(temp!=100){
                     ans=min(ans,1+temp);
                 }
             }
         }
-        return ans;
+        return dp[ind]=ans;
     }
     int minimumBeautifulSubstrings(string s) {
         int n=s.size();
-        int ans=solve(s,0);
+        vector<int>dp(n+1,-1);
+        int ans=solve(s,0,dp);
         if(ans>=100)return -1;
         return ans;
     }
