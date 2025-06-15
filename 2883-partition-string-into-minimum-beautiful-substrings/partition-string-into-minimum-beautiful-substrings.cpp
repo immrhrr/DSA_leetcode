005813da0deaaec306fb25d746ee7566d1e63b35
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-06-15 22:36:20 +0530

    Time: 62 ms (5.28%) | Memory: 29 MB (5.28%) - LeetSync

---
+++ 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool check(string&s,int start,int end){
+        if(s[start]=='0'||s[end]=='0')return false;
+        string temp;
+        unordered_set<string>st;
+        st.insert("11110100001001");
+        st.insert("110000110101");
+        st.insert("1");
+        st.insert("101");
+        st.insert("1001110001");
+        st.insert("1111101");
+        st.insert("11001");
+        
+        for(int i=start;i<=end;i++){
+            temp+=s[i];
+        }
+        if(st.find(temp)==st.end())return false;
+        return true;
+    }
+    int solve(string&s,int ind){
+        int n=s.size();
+        if(ind>=n)return 0;
+        int ans=100;
+        int temp=0;
+        for(int i=ind;i<n;i++){
+            if(check(s,ind,i)){
+                int temp=solve(s,i+1);
+                if(temp!=100){
+                    ans=min(ans,1+temp);
+                }
+            }
+        }
+        return ans;
+    }
+    int minimumBeautifulSubstrings(string s) {
+        int n=s.size();
+        int ans=solve(s,0);
+        if(ans>=100)return -1;
+        return ans;
+    }
+};
