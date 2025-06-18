959eac4a08eda6d3fd6ef78683790761241026c8
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-06-18 16:06:53 +0530

    Time: 2619 ms (5.07%) | Memory: 426.4 MB (5.07%) - LeetSync

---
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solve(vector<int>&cookies,int k,vector<int>temp,int ind,int&ans){
+        int n=cookies.size();
+        if(ind>=n){
+            int maxi=INT_MIN;
+            for(int i=0;i<k;i++){
+                maxi=max(maxi,temp[i]);
+            }
+            ans=min(ans,maxi);
+            return;
+        }
+        for(int op=0;op<k;op++){
+            temp[op]+=cookies[ind];
+            if(temp[op]<=ans){
+                solve(cookies,k,temp,ind+1,ans);
+            }
+            temp[op]-=cookies[ind];
+        }
+    }
+    int distributeCookies(vector<int>& cookies, int k) {
+        vector<int>temp(k,0);
+        int ans=INT_MAX;
+        solve(cookies,k,temp,0,ans);
+        return ans;
+    }
+};
