762f8f4aea72049f53778fca75cb3c56de025079
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-06-20 17:57:07 +0530

    Time: 59 ms (82.42%) | Memory: 38.2 MB (62.42%) - LeetSync

--- 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.cpp
+++ 3754-maximum-manhattan-distance-after-k-changes/maximum-manhattan-distance-after-k-changes.cpp
@@ -2,34 +2,20 @@ class Solution {
 public:
     int maxDistance(string s, int k) {
         int n=s.size();
+        int east=0;
+        int west=0;
+        int north=0;
+        int south=0;
         int ans=0;
-        int north=0;int east=0;int south=0;int west=0;
-        int x=0,y=0;
         for(int i=0;i<n;i++){
-            if(s[i]=='N'){
-                y++;
-                north++;
-            }
-             if(s[i]=='E'){
-                x++;
-                east++;
-            }
-             if(s[i]=='S'){
-                y--;
-                south++;
-            }
-             if(s[i]=='W'){
-                x--;
-                west++;
-            }
-            
-            int vn=min(north,south);
-            int hn=min(east,west);
-            int temp=abs(x)+abs(y);
-            int bonus=vn+hn;
-            bonus=(min(k,bonus));
-            temp+=2*bonus;
-            ans=max(ans,temp);
+            if(s[i]=='N')north++;
+            if(s[i]=='E')east++;
+            if(s[i]=='W')west++;
+            if(s[i]=='S')south++;
+            int expected=i+1;
+            int actual=abs(north-south)+abs(east-west);
+            int extra=min(2*k,expected-actual);
+            ans=max(ans,actual+extra);
         }
         return ans;
     }
