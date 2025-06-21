8afc90406f0325238784523c2c23eb0f000046c5
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-06-22 03:10:18 +0530

    Time: 4 ms (98.10%) | Memory: 12.6 MB (75.96%) - LeetSync

---
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    vector<int> largestDivisibleSubset(vector<int>& arr) {
+         sort(arr.begin(),arr.end());
+       int n=arr.size();
+       vector<int>lds(n,1);
+       vector<int>hash(n,0);
+       iota(hash.begin(),hash.end(),0);
+       int len=1;
+       int last_ind=n-1;
+       
+       for(int curr=0;curr<n;curr++){
+           for(int prev=0;prev<curr;prev++){
+               if(arr[curr]%arr[prev]==0&&lds[prev]+1>=lds[curr]){
+                   lds[curr]=1+lds[prev];
+                   hash[curr]=prev;
+               }
+               
+           }
+           if(len<=lds[curr]){
+               len=lds[curr];
+               last_ind=curr;
+           }
+       }
+       vector<int>ans;
+       while(hash[last_ind]!=last_ind){
+           ans.push_back(arr[last_ind]);
+           last_ind=hash[last_ind];
+       }
+        ans.push_back(arr[last_ind]);
+        reverse(ans.begin(),ans.end());
+       return ans;
+        
+    }
+};
