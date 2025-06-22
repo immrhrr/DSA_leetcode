e72148fb2ab23fa13b4cc738f8945120084cafd4
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-06-22 15:45:47 +0530

    Time: 970 ms (5.01%) | Memory: 395.4 MB (5.01%) - LeetSync

---
+++ 3903-inverse-coin-change/inverse-coin-change.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int solve(vector<bool>& vis, int start, int end, int need, vector<vector<int>>& dp) {
+        if (need == 0) return 1;
+        if (start >= end || need < 0) return 0;
+
+        if (dp[start][need] != -1) return dp[start][need];
+
+        int take = 0, notake = 0;
+
+        if (vis[start]) {
+            take = solve(vis, start, end, need - start, dp);
+        }
+
+        notake = solve(vis, start + 1, end, need, dp);
+
+        return dp[start][need] = take + notake;
+    }
+
+    vector<int> findCoins(vector<int>& ways) {
+        int n = ways.size();
+        vector<bool> ans(n + 1, false);
+
+        for (int i = 0; i < n; i++) {
+            // dp[start][need]
+            vector<vector<int>> dp(n + 2, vector<int>(i + 2, -1));
+            int cnt = solve(ans, 1, i + 1, i + 1, dp);
+
+            if (cnt + 1 == ways[i]) {
+                ans[i + 1] = true;
+            } else if (cnt != ways[i]) {
+                return {};
+            }
+        }
+
+        vector<int> res;
+        for (int i = 1; i <= n; i++) {
+            if (ans[i]) res.push_back(i);
+        }
+
+        return res;
+    }
+};
