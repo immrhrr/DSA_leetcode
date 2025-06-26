1bfb02c234f3cd32d47ad197666d0db7b5ff984d
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-06-26 18:01:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (33.33%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -3,16 +3,6 @@ public:
     int longestSubsequence(string s, int k) {
         int n=s.size();
         int length=0;
-         vector<int>pref(n,0);
-        if(s[0]=='0'){
-           pref[0]=1;
-        }
-        for(int i=1;i<n;i++){
-            pref[i]=pref[i-1];
-            if(s[i]=='0'){
-                pref[i]+=1;
-            }
-        }
         for(int i=n-1;i>=0;i--){
             if(s[i]=='0'){
                 length++;
@@ -25,6 +15,5 @@ public:
             }
         }
         return length;
-        
     }
 };
