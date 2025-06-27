3539ac008ede9154b794d0acc533a0b621e3aa4b
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-06-28 02:33:31 +0530

    Time: 4 ms (87.41%) | Memory: 126.3 MB (98.07%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
@@ -1,51 +1,43 @@
 class Solution {
 public:
     bool canPartitionGrid(vector<vector<int>>& grid) {
-        
-        int n=grid.size();
-        int m=grid[0].size();
-        long long tot_sum=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                tot_sum+=grid[i][j];
-            }
-        }
-        if(tot_sum%2==1)return false;
-        vector<long long>h_cut(n,0);
-        vector<long long>v_cut(m,0);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                h_cut[i]+=grid[i][j];
-            }
-        }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                v_cut[i]+=grid[j][i];
+        int n = grid.size();
+        int m = grid[0].size();
+
+        long long totalSum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                totalSum += grid[i][j];
             }
         }
 
-        //now try to partition h_cut or v_cut in the two equal partitons
-        //try horizontal cut
-        long long upar=h_cut[0];
-        long long neeche=tot_sum-upar;
-        for(int i=1;i<n;i++){
-            if(upar==neeche){
-                return true;
+        // If total sum is odd, equal partition is impossible
+        if (totalSum % 2 != 0) return false;
+
+        // Try horizontal cut (between rows)
+        long long topSum = 0;
+        for (int i = 0; i < n - 1; i++) { // cut after row i (i from 0 to n-2)
+            long long currentRowSum = 0;
+            for (int j = 0; j < m; j++) {
+                currentRowSum += grid[i][j];
             }
-            upar+=h_cut[i];
-            neeche-=h_cut[i];
+            topSum += currentRowSum;
+            long long bottomSum = totalSum - topSum;
+            if (topSum == bottomSum) return true;
         }
-        //try vertical cut
-        long long left=v_cut[0];
-        long long right=tot_sum-left;
-        for(int i=1;i<m;i++){
-            if(left==right){
-                return true;
+
+        // Try vertical cut (between columns)
+        long long leftSum = 0;
+        for (int j = 0; j < m - 1; j++) { // cut after column j (j from 0 to m-2)
+            long long currentColSum = 0;
+            for (int i = 0; i < n; i++) {
+                currentColSum += grid[i][j];
             }
-            left+=v_cut[i];
-            right-=v_cut[i];
+            leftSum += currentColSum;
+            long long rightSum = totalSum - leftSum;
+            if (leftSum == rightSum) return true;
         }
-        return false;
-        
+
+        return false; // No valid partition found
     }
 };
