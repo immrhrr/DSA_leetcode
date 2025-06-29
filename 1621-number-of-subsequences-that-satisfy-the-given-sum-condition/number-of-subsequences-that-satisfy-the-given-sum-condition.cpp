546623a0bb7b35faf0d1f8c6ef7120e8e9ddf4c9
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-06-29 13:27:36 +0530

    Time: 39 ms (24.94%) | Memory: 54.4 MB (14.79%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -5,7 +5,6 @@ public:
         int n = nums.size();
         int mod = 1e9 + 7;
 
-        // Precompute powers of 2
         vector<int> pow2(n, 1);
         for (int i = 1; i < n; i++) {
             pow2[i] = (pow2[i - 1] * 2) % mod;
@@ -15,10 +14,10 @@ public:
 
         for (int i = 0; i < n; i++) {
             int limit = target - nums[i];
-            if (limit < nums[i]) break; // no j such that nums[j] >= nums[i] and nums[i]+nums[j] <= target
+            if (limit < nums[i]) break; 
 
             int left = i, right = n - 1, ind = -1;
-            // binary search for maximum index `ind` where nums[ind] <= limit
+           
             while (left <= right) {
                 int mid = left + (right - left) / 2;
                 if (nums[mid] <= limit) {
