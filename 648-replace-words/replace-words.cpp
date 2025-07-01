2fd398941379f5f3493b38b4cfec00ca40f692a3
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-07-01 23:32:44 +0530

    Time: 35 ms (75.43%) | Memory: 71.5 MB (83.18%) - LeetSync

---
+++ 648-replace-words/replace-words.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    struct trieNode {
+        bool is_end;
+        string word;
+        trieNode* children[26];
+    };
+
+    trieNode* getNode() {
+        trieNode* newNode = new trieNode();
+        for (int i = 0; i < 26; i++) {
+            newNode->children[i] = nullptr;
+        }
+        newNode->is_end = false;
+        newNode->word = "";
+        return newNode;
+    }
+
+    void insert(string& word, trieNode* root) {
+        trieNode* crawl = root;
+        for (char ch : word) {
+            int idx = ch - 'a';
+            if (!crawl->children[idx]) {
+                crawl->children[idx] = getNode();
+            }
+            crawl = crawl->children[idx];
+        }
+        crawl->is_end = true;
+        crawl->word = word;
+    }
+
+    string search(string& word, trieNode* root) {
+        trieNode* crawl = root;
+        for (char ch : word) {
+            int idx = ch - 'a';
+            if (!crawl->children[idx]) {
+                return word;
+            }
+            crawl = crawl->children[idx];
+            if (crawl->is_end) {
+                return crawl->word;
+            }
+        }
+        return word;
+    }
+
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        trieNode* root = getNode();
+        for (string word : dictionary) {
+            insert(word, root);
+        }
+
+        string ans = "";
+        string temp = "";
+
+        for (int i = 0; i < sentence.size(); i++) {
+            if (sentence[i] == ' ') {
+                ans += search(temp, root);
+                ans += " ";
+                temp = "";
+            } else {
+                temp += sentence[i];
+            }
+        }
+
+        // Add last word (outside the loop)
+        ans += search(temp, root);
+
+        return ans;
+    }
+};
