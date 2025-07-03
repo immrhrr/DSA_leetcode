0ba8a84150672e8bf5d1465d5df2399047cf5cd4
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-07-04 02:07:42 +0530

    Time: 235 ms (62.26%) | Memory: 114.5 MB (68.32%) - LeetSync

---
+++ 3356-shortest-uncommon-substring-in-an-array/shortest-uncommon-substring-in-an-array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<string> shortestSubstrings(vector<string>& arr) {
+        int n = arr.size();
+        unordered_map<string, int> globalFreq;
+        vector<unordered_set<string>> individual(n);
+
+        // Step 1: Build global and individual substring maps
+        for (int i = 0; i < n; ++i) {
+            int len = arr[i].length();
+            for (int start = 0; start < len; ++start) {
+                string temp = "";
+                for (int end = start; end < len; ++end) {
+                    temp += arr[i][end];
+                    if (individual[i].insert(temp).second) {
+                        globalFreq[temp]++;
+                    }
+                }
+            }
+        }
+
+        vector<string> ans(n);
+        for (int i = 0; i < n; ++i) {
+            string res = "";
+            int minLen = INT_MAX;
+
+            for (const string& s : individual[i]) {
+                if (globalFreq[s] == 1) {
+                    int slen = s.length();
+                    if (slen < minLen || (slen == minLen && s < res)) {
+                        res = s;
+                        minLen = slen;
+                    }
+                }
+            }
+
+            ans[i] = res;
+        }
+
+        return ans;
+    }
+};
