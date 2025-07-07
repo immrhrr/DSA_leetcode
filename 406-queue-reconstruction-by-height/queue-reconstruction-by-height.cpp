c5a071c8d2bf74144773598fc21177089d06e217
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-07-07 16:41:24 +0530

    Time: 24 ms (26.10%) | Memory: 16.5 MB (27.42%) - LeetSync

--- 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
@@ -9,26 +9,29 @@ public:
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
         sort(people.begin(),people.end(),comp);
         int n=people.size();
-        vector<vector<int>>ans(n);
-        stack<vector<int>>st;
-        stack<vector<int>>temp;
-        for(int i=0;i<n;i++){
-            while(st.size()!=people[i][1]){
-                auto it=st.top();
-                temp.push(it);
-                st.pop();
-            }
-            st.push(people[i]);
-            while(!temp.empty()){
-                st.push(temp.top());
-                temp.pop();
-            }
+        vector<vector<int>>ans;
+        for(auto p:people){
+            ans.insert(ans.begin()+p[1],p);
         }
-        int ind=n-1;
-        while(!st.empty()){
-            ans[ind--]=st.top();
-            st.pop();
-        }
+        // stack<vector<int>>st;
+        // stack<vector<int>>temp;
+        // for(int i=0;i<n;i++){
+        //     while(st.size()!=people[i][1]){
+        //         auto it=st.top();
+        //         temp.push(it);
+        //         st.pop();
+        //     }
+        //     st.push(people[i]);
+        //     while(!temp.empty()){
+        //         st.push(temp.top());
+        //         temp.pop();
+        //     }
+        // }
+        // int ind=n-1;
+        // while(!st.empty()){
+        //     ans[ind--]=st.top();
+        //     st.pop();
+        // }
         return ans;
     }
 };
