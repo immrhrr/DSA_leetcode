4d5f8cdf817a9062c497eba434f72e8766c72b56
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-07-07 08:00:55 +0530

    Time: 267 ms (9.00%) | Memory: 115.6 MB (10.36%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -9,17 +9,22 @@ public:
     int maxEvents(vector<vector<int>>& events) {
         sort(events.begin(), events.end(), comp);
         set<int> st;
-        for (int i = 1; i <= 1e5 + 1; i++) {
+        int n = events.size();
+        int mini=1e5+1;
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            mini=min(events[i][0],mini);
+            maxi=max(events[i][1],maxi);
+
+        }
+        for (int i = mini; i <= maxi; i++) {
             st.insert(i);
         }
         int ans = 0;
-        int n = events.size();
+        
         for (int i = 0; i < n; i++) {
             int a = events[i][0];
             int b = events[i][1];
-            // find the smallest number greater than or equal to a and smaller
-            // than equal to b in the set st then erase that val from set and
-            // ans++;
             auto it1 = st.lower_bound(a);
             if (it1 != st.end()) {
                if(*it1<=b){
