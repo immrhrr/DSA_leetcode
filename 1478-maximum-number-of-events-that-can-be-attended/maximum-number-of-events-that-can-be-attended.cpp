6b03d72cff8fcd4a47c200fdd61fc13ec3c3df10
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-07-08 14:07:46 +0530

    Time: 250 ms (9.47%) | Memory: 115.6 MB (9.08%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -8,6 +8,9 @@ public:
     }
     int maxEvents(vector<vector<int>>& events) {
         sort(events.begin(), events.end(), comp);
+       //jo pehle khatm ho rha pehle usko try kro
+       //agar dono same time pe khatm ho rha toh jo pehle aaya usko pehle
+       //sort(events.begin(),events.end());
         set<int> st;
         int n = events.size();
         int mini=1e5+1;
@@ -15,21 +18,19 @@ public:
         for(int i=0;i<n;i++){
             mini=min(events[i][0],mini);
             maxi=max(events[i][1],maxi);
-
         }
         for (int i = mini; i <= maxi; i++) {
             st.insert(i);
         }
         int ans = 0;
-        
         for (int i = 0; i < n; i++) {
-            int a = events[i][0];
-            int b = events[i][1];
-            auto it1 = st.lower_bound(a);
-            if (it1 != st.end()) {
-               if(*it1<=b){
+            int start = events[i][0];
+            int end = events[i][1];
+            auto it = st.lower_bound(start);
+            if (it != st.end()) {
+               if(*it<=end){
                 ans++;
-                st.erase(*it1);
+                st.erase(*it);
                }
             }
         }
