287268c3b884d9fd3e5e5c276a3a359f4e39ae92
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-07-08 21:58:42 +0530

    Time: 560 ms (37.77%) | Memory: 306.4 MB (25.18%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.cpp
@@ -3,34 +3,35 @@ public:
     vector<int> ans;
 
     void merge(vector<pair<int, int>>& nums, int l, int mid, int r) {
-        vector<pair<int, int>> left(nums.begin() + l, nums.begin() + mid + 1);
-        vector<pair<int, int>> right(nums.begin() + mid + 1, nums.begin() + r + 1);
+        // --- 1) COUNT PASS (minimal addition) ---
+        // Both halves nums[l..mid] and nums[mid+1..r] are sorted ascending from recursion
+        int j = mid + 1;
+        for (int i = l; i <= mid; ++i) {
+            // advance j while right[j] < left[i]
+            while (j <= r && nums[i].first > nums[j].first) {
+                ++j;
+            }
+            // all elements from mid+1 .. (j-1) are smaller than nums[i]
+            ans[nums[i].second] += (j - (mid + 1));
+        }
 
-        int n1 = left.size();
-        int n2 = right.size();
-        int i = 0, j = 0;
-        int ind = l;
-        int countRight = 0;
+        // --- 2) YOUR ORIGINAL MERGE (ascending order) ---
+        vector<pair<int,int>> left(nums.begin()+l,      nums.begin()+mid+1);
+        vector<pair<int,int>> right(nums.begin()+mid+1, nums.begin()+r+1);
+
+        int i = 0, k = l;
+        j = 0;
+        int n1 = left.size(), n2 = right.size();
 
         while (i < n1 && j < n2) {
             if (left[i].first <= right[j].first) {
-                // All previous right[j]s are smaller than left[i]
-                ans[left[i].second] += countRight;
-                nums[ind++] = left[i++];
+                nums[k++] = left[i++];
             } else {
-                // right[j] < left[i]
-                countRight++;
-                nums[ind++] = right[j++];
+                nums[k++] = right[j++];
             }
         }
-
-        while (i < n1) {
-            ans[left[i].second] += countRight;
-            nums[ind++] = left[i++];
-        }
-        while (j < n2) {
-            nums[ind++] = right[j++];
-        }
+        while (i < n1) nums[k++] = left[i++];
+        while (j < n2) nums[k++] = right[j++];
     }
 
     void mergesort(vector<pair<int, int>>& nums, int l, int r) {
@@ -43,11 +44,12 @@ public:
 
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
-        ans.resize(n, 0);
-        vector<pair<int, int>> temp;
-        for (int i = 0; i < n; i++) {
-            temp.push_back({nums[i], i});
-        }
+        ans.assign(n, 0);
+        vector<pair<int,int>> temp;
+        temp.reserve(n);
+        for (int i = 0; i < n; ++i)
+            temp.emplace_back(nums[i], i);
+
         mergesort(temp, 0, n - 1);
         return ans;
     }
