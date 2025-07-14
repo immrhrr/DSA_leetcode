a08d4005418db8aa925a8a3a3c3ff7312db95507
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-07-14 22:18:06 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (14.18%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,49 +1,47 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n=s.size();
-        stack<string>st;
-        string temp="";
-        int startInd=-1;
-        int endInd=n;
-        for(int i=0;i<n;i++){
-            if(s[i]!=' '){
-                startInd=i;
+        int n = s.size();
+        string temp = "";
+        int startInd = -1;
+        int endInd = n;
+        for (int i = 0; i < n; i++) {
+            if (s[i] != ' ') {
+                startInd = i;
                 break;
             }
         }
-        for(int i=n-1;i>=0;i--){
-            if(s[i]!=' '){
-                endInd=i;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] != ' ') {
+                endInd = i;
                 break;
             }
         }
-        if(startInd==-1)return "";
-
-        for(int i=startInd;i<=endInd;i++){
-            if(s[i]!=' '){
-                temp+=s[i];
-            }
-            else{
-                if(temp.size()>0)
-                st.push(temp);
-                temp="";
+        if (startInd == -1)
+            return "";
+        string ans = "";
+        for (int i = startInd; i <= endInd; i++) {
+            if (s[i] != ' ') {
+                temp += s[i];
+            } else {
+                if (temp.size() > 0) {
+                    if (ans.size() == 0) {
+                        ans = temp;
+                    } else {
+                        ans = temp + ' ' + ans;
+                    }
+                    temp = "";
+                }
             }
         }
-        string ans="";
-        if(temp.size()>0)
-        st.push(temp);
 
-        while(!st.empty()){
-            if(ans.size()==0){
-                ans+=st.top();
+        if (temp.size() > 0) {
+            if (ans.size() == 0) {
+                ans = temp;
+            } else {
+                ans = temp + ' ' + ans;
             }
-            else{
-                ans+=" ";
-                ans+=st.top();
-            }
-            st.pop();
         }
-        return ans;   
+        return ans;
     }
 };
