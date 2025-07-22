d2ddfd0ac4b6a00272afb6642eb4c4d37ebd9fae
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-07-22 19:44:27 +0530

    Time: 97 ms (50.20%) | Memory: 95 MB (47.17%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -5,9 +5,9 @@ public:
         vector<vector<int>>dp(n+2,vector<int>(2,0));
         for(int i=n-1;i>=0;i--){
             //canbuy
-            dp[i][0]=max(-prices[i]-fee+dp[i+1][1],dp[i+1][0]);
+            dp[i][0]=max(-prices[i]+dp[i+1][1],dp[i+1][0]);
             //cansell
-            dp[i][1]=max(prices[i]+dp[i+1][0],dp[i+1][1]);
+            dp[i][1]=max(prices[i]-fee+dp[i+1][0],dp[i+1][1]);
         }
         return dp[0][0];
         
