2b3454817389eff72f0d7ccc39e31ef992921852
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-07-22 16:18:44 +0530

    Time: 196 ms (32.30%) | Memory: 131.1 MB (31.90%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        // basically we want to find the max sum of a subarray which has all
-        // unique elements
+        unordered_set<int> seen;
         int n = nums.size();
-        int ans = 0;
-        int i = 0;
-        int j = 0;
-        unordered_map<int, int> mp;
-        int sum = 0;
+        int i = 0, j = 0;
+        int sum = 0, maxSum = 0;
+
         while (j < n) {
-            sum += nums[j];
-            if (mp.find(nums[j]) != mp.end()) {
-                while (i < j && nums[i] != nums[j]) {
-                    sum -= nums[i];
-                    mp[nums[i]]--;
-                    if (mp[nums[i]] == 0) {
-                        mp.erase(nums[i]);
-                    }
-                    i++;
-                }
-                if (nums[i] == nums[j]) {
-                    sum -= nums[i];
-                    mp[nums[i]]--;
-                    if (mp[nums[i]] == 0) {
-                        mp.erase(nums[i]);
-                    }
-                    i++;
-                }
+            while (seen.count(nums[j])) {
+                seen.erase(nums[i]);
+                sum -= nums[i];
+                i++;
             }
-            mp[nums[j]]++;
-            if (mp.size() == j - i + 1)
-                ans = max(ans, sum);
+            seen.insert(nums[j]);
+            sum += nums[j];
+            maxSum = max(maxSum, sum);
             j++;
         }
-        return ans;
+
+        return maxSum;
     }
 };
