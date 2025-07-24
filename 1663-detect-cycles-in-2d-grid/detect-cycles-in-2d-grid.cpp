3065852ba4a4289e1f7ca32b60585d1333a4143c
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-07-24 15:55:19 +0530

    Time: 319 ms (22.42%) | Memory: 253.2 MB (5.22%) - LeetSync

---
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, -1, 0, 1};
+    int n = 0;
+    int m = 0;
+
+    bool bfs(int r, int c, vector<vector<char>>& grid, vector<vector<bool>>& vis) {
+        char ch = grid[r][c];
+        vis[r][c] = true;
+        queue<vector<int>> q;
+        q.push({r, c, -1, -1});  // row, col, parent_row, parent_col
+
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+
+            int row = it[0];
+            int col = it[1];
+            int prow = it[2];
+            int pcol = it[3];
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + dx[i];
+                int ncol = col + dy[i];
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                    grid[nrow][ncol] == ch) {
+                    
+                    if (nrow == prow && ncol == pcol) continue;  // don't revisit parent
+                    
+                    if (vis[nrow][ncol]) {
+                        return true;  // found cycle
+                    } else {
+                        vis[nrow][ncol] = true;
+                        q.push({nrow, ncol, row, col});
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    bool containsCycle(vector<vector<char>>& grid) {
+        n = grid.size();
+        m = grid[0].size();
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (vis[i][j] == false) {
+                    if (bfs(i, j, grid, vis)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
