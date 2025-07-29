2c509d07d03b583dca10d68691e7f61499229a01
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-07-29 23:22:52 +0530

    Time: 4 ms (74.34%) | Memory: 12.3 MB (61.58%) - LeetSync

---
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int solve(vector<int>& arr, int i, int j, vector<vector<int>>& dp,vector<vector<int>>&memo) {
+        if (i >= j) {
+            return 0; 
+        }
+        if(memo[i][j]!=-1)return  memo[i][j];
+        int ans = INT_MAX;
+        for (int k = i; k < j; k++) {
+            int temp = dp[i][k] * dp[k + 1][j] + solve(arr, i, k, dp,memo) + solve(arr, k + 1, j, dp,memo);
+            ans = min(temp, ans);
+        }
+        return memo[i][j]= ans;
+    }
+
+    int mctFromLeafValues(vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0)); 
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = arr[i];
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = max(dp[i][j - 1], arr[j]);
+            }
+        }
+        vector<vector<int>>memo(n,vector<int>(n,-1));
+        return solve(arr, 0, n - 1, dp,memo);
+    }
+};
