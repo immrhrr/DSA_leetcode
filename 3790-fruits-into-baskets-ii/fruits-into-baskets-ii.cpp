c11a38e82f24d1a6fbefa4f54a3c8589021b6518
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-05 16:44:00 +0530

    Time: 4 ms (35.77%) | Memory: 48.9 MB (75.84%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
         int n=fruits.size();
-        vector<int>vis(n,-1);
-        int cnt=0;
+        int filled=0;
         for(int i=0;i<n;i++){
-            int need=fruits[i];
+            int curr=fruits[i];
             for(int j=0;j<n;j++){
-                if(vis[j]==1){
-                    continue;
-                }
-                int available=baskets[j];
-                if(need<=available){
-                    vis[j]=1;
-                    cnt++;
+                if(baskets[j]>=curr){
+                    baskets[j]=0;
+                    filled++;
                     break;
                 }
             }
         }
+        return n-filled;
         
-        
-        return n-cnt;
     }
 };
