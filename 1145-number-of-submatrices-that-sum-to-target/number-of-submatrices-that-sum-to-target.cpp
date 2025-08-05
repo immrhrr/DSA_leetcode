07ec38c3ebc98dfef06a6fc561d26a49c2ca03aa
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-05 22:16:47 +0530

    Time: 439 ms (78.90%) | Memory: 155.2 MB (54.19%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
@@ -4,19 +4,20 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         int cnt = 0;
+        //cumulative sum ka matrix banao column wise
         vector<vector<int>> pref(n, vector<int>(m, 0));
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 pref[j][i] = matrix[j][i] + (i > 0 ? pref[j][i - 1] : 0);
             }
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = i; j < m; j++) {
+        for (int start_col = 0; start_col < m; start_col++) {
+            for (int end_col = start_col; end_col < m; end_col++) {
                 unordered_map<int, int> mp;
                 mp[0] = 1;
                 int curr=0;
-                for (int k = 0; k < n; k++) {
-                    curr+=pref[k][j]-(i>0?pref[k][i-1]:0);
+                for (int row = 0; row < n; row++) {
+                    curr+=pref[row][end_col]-(start_col>0?pref[row][start_col-1]:0);
                     int need=curr-target;
                     if(mp.find(need)!=mp.end()){
                         cnt+=mp[need];
