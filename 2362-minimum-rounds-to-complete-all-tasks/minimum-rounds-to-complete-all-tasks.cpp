8393a256477cdf94da532ba19303c3fd0b5356e7
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-06 22:34:15 +0530

    Time: 33 ms (45.81%) | Memory: 107.5 MB (52.90%) - LeetSync

--- 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.cpp
+++ 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
-    bool possible(unordered_map<int,int>&mp,int limit){
-        int temp=0;
-        for(auto it:mp){
-            int val=it.second;
-            temp+=(val+2)/3;
-        }
-        return temp<=limit;
-
-    }
+   
     int minimumRounds(vector<int>& tasks) {
-        unordered_map<int,int>mp;
-        for(int i=0;i<tasks.size();i++){
+        unordered_map<int, int> mp;
+        for (int i = 0; i < tasks.size(); i++) {
             mp[tasks[i]]++;
         }
-        for(auto it:mp){
-            if(it.second==1)return -1;
+        int temp = 0;
+        for (auto it : mp) {
+            int val = it.second;
+            if(val==1)return -1;
+            temp += (val + 2) / 3;
         }
-        int low=1;
-        int high=tasks.size()-1;
-        int mid=0;
-        int ans=-1;
-        while(low<=high){
-            mid=low+(high-low)/2;
-            if(possible(mp,mid)){
-                ans=mid;
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-        }
-        return ans;
-        
+        return temp;
+       
     }
 };
