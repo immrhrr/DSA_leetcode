dfe84aa5b1fec1b487b8c5a72c91258a861bd54e
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-07 17:59:38 +0530

    Time: 129 ms (58.43%) | Memory: 114.5 MB (59.81%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
-        vector<vector<pair<int, int>>> adj(n);
+        vector<vector<pair<int,int>>> adj(n);
         for (auto it : connections) {
             int u = it[0];
             int v = it[1];
@@ -9,27 +9,25 @@ public:
             adj[v].push_back({u, 0});
         }
         // 1-> asli edge
-        // 0->nakli edges
-        vector<bool> vis(n, false);
+        // 0-> nakli edge
+        int cnt = 0;
         queue<int> q;
         q.push(0);
+        vector<bool> vis(n, false);
         vis[0] = true;
-        int cnt = 0;
-        // if we use nakli edge to go away from 0 then it is okay
-        // but we we use the asli edge to go away from 0 then cnt++;
+        // if we use alsi edge to go away from 0 then cnt++
+        // if we use nakli edge to go away then its fine
         while (!q.empty()) {
             int curr = q.front();
             q.pop();
             for (int i = 0; i < adj[curr].size(); i++) {
                 int ngbr = adj[curr][i].first;
-                int check=adj[curr][i].second;
-                if (!vis[ngbr]) {
-                    vis[ngbr] = true;
-                    if(check==1){
-                         cnt++;
+                if (vis[ngbr] == false) {
+                    if (adj[curr][i].second == 1) {
+                        cnt++;
                     }
-                    q.push(ngbr);
-                }
+                    vis[ngbr] = true;
+                    q.push(ngbr);                }
             }
         }
         return cnt;
