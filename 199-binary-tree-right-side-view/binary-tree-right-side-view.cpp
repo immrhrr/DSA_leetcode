8e4084e14903c09913361f27b80ce541548cae69
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-07 16:49:41 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (40.09%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,29 +11,20 @@
  */
 class Solution {
 public:
+    void solve(TreeNode*curr,vector<int>&ans,int level){
+        if(!curr)return;
+        if(level-1==ans.size())
+        ans.push_back(curr->val);
+        solve(curr->right,ans,level+1);
+        solve(curr->left,ans,level+1);
+
+    }
     vector<int> rightSideView(TreeNode* root) {
         if(!root){
             return {};
         }
         vector<int>ans;
-        queue<TreeNode*>q;
-        TreeNode* curr=NULL;
-        q.push(root);
-        while(!q.empty()){
-            int m=q.size();
-            ans.push_back(q.front()->val);
-            while(m--){
-                curr=q.front();
-                q.pop();
-                if(curr->right){
-                    q.push(curr->right);
-                }
-                if(curr->left){
-                    q.push(curr->left);
-                }
-            }
-        }
+        solve(root,ans,1);
         return ans;
-        
     }
 };
