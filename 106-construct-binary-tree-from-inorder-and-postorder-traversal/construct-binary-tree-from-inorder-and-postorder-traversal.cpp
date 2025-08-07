d2f56d53441d55236349e3687234b1469d660c33
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-07 19:56:44 +0530

    Time: 4 ms (51.07%) | Memory: 27.6 MB (38.50%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -6,36 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* Tree(vector<int>& inorder, vector<int>& postorder,int inStart,int inEnd,int index,unordered_map<int,int>&mp){
-        if(inStart>inEnd){
+    TreeNode* build(vector<int>& postorder, vector<int>& inorder, int inStart,
+                    int inEnd, int ind, unordered_map<int, int>& mp) {
+        if (inStart > inEnd)
             return NULL;
-        }
-        TreeNode* root=new TreeNode(postorder[index]);
-        int position=mp[postorder[index]];
-       
-        root->left=Tree(inorder,postorder,inStart,position-1,index-(inEnd-position)-1,mp);
-         root->right=Tree(inorder,postorder,position+1,inEnd,index-1,mp);
+        TreeNode* root = new TreeNode(postorder[ind]);
+        int pos = mp[postorder[ind]];
+        root->right = build(postorder, inorder, pos + 1, inEnd,
+                            ind - 1, mp);
+        root->left = build(postorder, inorder, inStart, pos - 1, ind - (inEnd - pos) - 1, mp);
+
         return root;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int n=inorder.size();
-        if(n==0){
-            return NULL;
+        unordered_map<int, int> mp;
+        int n = inorder.size();
+        for (int i = 0; i < n; i++) {
+            mp[inorder[i]] = i;
         }
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
-            mp[inorder[i]]=i;
-        }
-        int inStart=0;
-        int inEnd=n-1;
-        int index=n-1;
-
-        
-        return Tree(inorder,postorder,inStart,inEnd,index,mp);
+        return build(postorder, inorder, 0, n - 1, n-1, mp);
     }
 };
