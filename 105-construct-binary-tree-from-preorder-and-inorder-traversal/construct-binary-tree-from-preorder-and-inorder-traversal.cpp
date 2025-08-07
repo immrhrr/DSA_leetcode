9fb8a12884b77128a06c495d2b17fe0d2d8fc83e
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-07 19:32:10 +0530

    Time: 5 ms (45.18%) | Memory: 27.5 MB (37.59%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,25 +11,20 @@
  */
 class Solution {
 public:
-     TreeNode* Tree(vector<int>&inorder,vector<int>&preorder,int inStart,
-  int inEnd,int index,unordered_map<int,int>&mp){
-      if(inStart>inEnd){
-          return NULL;
-      }
-      TreeNode* root=new TreeNode(preorder[index]);
-      int position=mp[preorder[index]];
-      root->left=Tree(inorder,preorder,inStart,position-1,index+1,mp);
-      root->right=Tree(inorder,preorder,position+1,inEnd,index+(position-inStart)+1,mp);
-      return root;
-  }
+    TreeNode* build(vector<int>&preorder,vector<int>&inorder,int inStart,int inEnd,int ind, unordered_map<int,int>&mp){
+        if(inStart>inEnd)return NULL;
+        TreeNode* root=new TreeNode(preorder[ind]);
+        int pos=mp[preorder[ind]];
+        root->left=build(preorder,inorder,inStart,pos-1,ind+1,mp);
+        root->right=build(preorder,inorder,pos+1,inEnd,ind+(pos-inStart)+1,mp);
+        return root;
+    }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        if(inorder.size()==0){
-           return NULL;
-       }
-       unordered_map<int,int>mp;
-       for(int i=0;i<inorder.size();i++){
-           mp[inorder[i]]=i;
-       }
-       return Tree(inorder,preorder,0,inorder.size()-1,0,mp);
+        unordered_map<int,int>mp;
+        int n=inorder.size();
+        for(int i=0;i<n;i++){
+            mp[inorder[i]]=i;
+        }
+        return build(preorder,inorder,0,n-1,0,mp);
     }
 };
