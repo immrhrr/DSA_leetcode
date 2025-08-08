0a2be91eb46e024d71c57c0ef39f23c9bf4541fc
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-08 06:27:31 +0530

    Time: 2 ms (68.69%) | Memory: 23.7 MB (19.53%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,44 @@
+class MinStack {
+public:
+    stack<long long> st;
+    long long mini;
+
+    MinStack() {
+        mini = 0; // Will be set on first push
+    }
+
+    void push(int val) {
+        if (st.empty()) {
+            st.push(val);
+            mini = val;
+        } else if (val >= mini) {
+            st.push(val);
+        } else {
+            st.push(2LL * val - mini); // store modified value
+            mini = val;
+        }
+    }
+
+    void pop() {
+        if (st.empty()) return;
+        if (st.top() >= mini) {
+            st.pop();
+        } else {
+            mini = 2LL * mini - st.top(); // restore previous min
+            st.pop();
+        }
+    }
+
+    int top() {
+        if (st.empty()) return -1;
+        if (st.top() >= mini) {
+            return st.top();
+        } else {
+            return mini; // encoded value case
+        }
+    }
+
+    int getMin() {
+        return mini;
+    }
+};
