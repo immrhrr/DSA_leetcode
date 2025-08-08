a641fc57b06bf3a88d08a2a03454d0d5a4e34ff1
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-08 22:35:30 +0530

    Time: 128 ms (19.50%) | Memory: 428 MB (25.67%) - LeetSync

--- 826-soup-servings/soup-servings.cpp
+++ 826-soup-servings/soup-servings.cpp
@@ -22,9 +22,8 @@ public:
         return dp[a][b]= 0.25*p;
     }
     double soupServings(int n) {
-        if(n>5000)return 1;
+        if(n>4275)return 1;
         dp.resize(n+1,vector<double>(n+1,-1.0));
-        
         return solve(n,n);
     }
 };
