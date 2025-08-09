f115306f5e0ff2dc6296be65d7355de8058d6849
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-09 18:53:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (17.71%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
+        return n>0&&(n & (n - 1)) == 0;
     }
+    // technologia: n & n-1 is zero then n is a power of 2
 };
