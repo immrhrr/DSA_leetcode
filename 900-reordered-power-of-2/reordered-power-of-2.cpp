8500a1531f23f2c9242cbbcd2ef19b0a6b30fbc3
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-10 18:12:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (14.75%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    unordered_set<string> st;
+    void helper() {
+        string s = "";
+        for (int i = 0; i <= 30; i++) {
+            s = to_string(1 << i);
+            sort(s.begin(), s.end());
+            st.insert(s);
+        }
+
+        return;
+    }
+    bool reorderedPowerOf2(int n) {
+        helper();
+        string temp = to_string(n);
+        sort(temp.begin(), temp.end());
+        if (st.find(temp) != st.end())
+            return true;
+        return false;
+    }
+};
