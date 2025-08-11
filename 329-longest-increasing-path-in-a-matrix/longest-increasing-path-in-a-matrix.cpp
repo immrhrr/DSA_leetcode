cb928b386e91af473ada34ffd0921033d24b505b
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-12 03:43:06 +0530

    Time: 8 ms (78.26%) | Memory: 20.8 MB (77.20%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>&matrix,int r,int c,vector<vector<int>>&dp){
-        if(dp[r][c]!=-1)return dp[r][c];
-        int m=matrix.size();
-        int n=matrix[0].size();
-        vector<int>h={1,0,-1,0};
-        vector<int>v={0,-1,0,1};
+    vector<int>dx={1,0,-1,0};
+    vector<int>dy={0,-1,0,1};
+    vector<vector<int>>dp;
+    int dfs(vector<vector<int>>&matrix,int row,int col){
+        int n=matrix.size();
+        int m=matrix[0].size();
+        if(dp[row][col]!=-1)return dp[row][col];
         int temp=1;
         for(int i=0;i<4;i++){
-            int nrow=r+h[i];
-            int ncol=c+v[i];
-            if(nrow>=0&&nrow<m&&ncol>=0&&ncol<n&&matrix[nrow][ncol]>matrix[r][c]){
-                temp=max(temp,1+dfs(matrix,nrow,ncol,dp));
+            int nrow=row+dx[i];
+            int ncol=col+dy[i];
+            if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&matrix[nrow][ncol]>matrix[row][col]){
+                temp=max(temp,1+dfs(matrix,nrow,ncol));
             }
         }
-        return dp[r][c]=temp;
+        return dp[row][col]= temp;
+
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        int n=matrix.size();
+        int m=matrix[0].size();
+        dp.resize(n,vector<int>(m,-1));
         int ans=1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                ans=max(ans,dfs(matrix,i,j,dp));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(dp[i][j]==-1){
+                   ans=max(ans,dfs(matrix,i,j));
+                }
             }
         }
         return ans;
-        
     }
 };
