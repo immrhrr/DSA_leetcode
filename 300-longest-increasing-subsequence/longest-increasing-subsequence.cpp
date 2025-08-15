1f5ef2e3ab6bc3c0fa117ee543364789451dc638
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-16 03:22:32 +0530

    Time: 75 ms (55.43%) | Memory: 14.2 MB (73.85%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -2,32 +2,18 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<int>temp;
-        temp.push_back(INT_MIN);
+        vector<int>lis(n,1);
+        int ans=1;
+        //length of lis ending at ith index
         for(int i=0;i<n;i++){
-            if(nums[i]>temp.back()){
-                temp.push_back(nums[i]);
-            }
-            else{
-                //find the smallest number that is bigger than nums[i];
-                //ans replace it with this
-                int low=0;
-                int high=temp.size()-1;
-                int index=0;
-                while(low<=high){
-                    int mid=low+(high-low)/2;
-                    if(temp[mid]>=nums[i]){
-                        index=mid;
-                        high=mid-1;
-                    }
-                    else{
-                      low=mid+1;
-                    }
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    lis[i]=max(1+lis[j],lis[i]);
                 }
-                temp[index]=nums[i];
+                
+                ans=max(ans,lis[i]);
             }
         }
-        return temp.size()-1;
-        
+        return ans;
     }
 };
