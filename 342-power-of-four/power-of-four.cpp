88f38b7f9e86bcd602edea5a6f73c22a739fa734
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-15 21:00:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (18.14%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n<=0)return false;
-        vector<int>arr;
-        while(n>0){
-            arr.push_back(n%4);
-            n=n/4;
-        }
-        reverse(arr.begin(),arr.end());
-        if(arr[0]!=1)return false;
-        for(int i=1;i<arr.size();i++){
-            if(arr[i]!=0)return false;
-
-        }
-        return true;
+        return n > 0 &&
+               (n & (n - 1)) == 0 &&   // power of two
+               (n & 0x55555555) != 0; // bit at odd position
     }
 };
