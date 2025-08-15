50ad3fb78a77efc6b9f3ce3a7dee18419d55ea92
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-16 04:39:26 +0530

    Time: 55 ms (29.93%) | Memory: 92.2 MB (15.97%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     static bool comp(const vector<int>& a, const vector<int>& b) {
-        if (a[0] == b[0]) return a[1] > b[1];
-        return a[0] < b[0];
+        if (a[0] == b[0])
+            return a[1] > b[1]; // sort heights in descending order if widths are equal
+        return a[0] < b[0];     // otherwise sort by width ascending
     }
 
     int helper(int w, vector<vector<int>>& temp) {
         int low = 0, high = temp.size() - 1;
-        int ind = temp.size(); // default position = end
+        int ind = temp.size(); // default = insert at end
+
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (temp[mid][1] >= w) {
@@ -23,9 +25,9 @@ public:
     int maxEnvelopes(vector<vector<int>>& env) {
         int n = env.size();
         sort(env.begin(), env.end(), comp);
-        vector<vector<int>> temp;
+
+        vector<vector<int>> temp; // will store LIS on heights
         for (int i = 0; i < n; i++) {
-            int l = env[i][0];
             int w = env[i][1];
             if (temp.empty() || w > temp.back()[1]) {
                 temp.push_back(env[i]);
