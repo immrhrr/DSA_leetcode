5f51f6f992f0c31c88b3fb0680bee7b6392620ff
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-16 00:26:45 +0530

    Time: 34 ms (37.93%) | Memory: 27.1 MB (51.19%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-
-    int solve(string &s, string &t, int i, int j) {
-        // i = length of s considered, j = length of t considered
-        if (j == 0) return 1;  // matched all of t
-        if (i == 0) return 0;  // ran out of s before matching t
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int take = 0, notake = 0;
-        if (s[i - 1] == t[j - 1]) { // match last chars
-            take = solve(s, t, i - 1, j - 1);
+    vector<vector<int>>dp;
+    int solve(string&s,string&t,int i,int j){
+        if(j<0)return 1;
+        if(i<0)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int take=0;
+        int notake=0;
+        if(s[i]==t[j]){
+            take=solve(s,t,i-1,j-1);
         }
-        notake = solve(s, t, i - 1, j);
-
-        return dp[i][j] = take + notake;
+        notake=solve(s,t,i-1,j);
+        return dp[i][j]= take+notake;
+        
     }
-
     int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        dp.assign(n + 1, vector<int>(m + 1, -1));
-        return solve(s, t, n, m); // pass lengths
+        int n=s.size();
+        int m=t.size();
+        dp.resize(n+1,vector<int>(m+1,-1));
+        return solve(s,t,n-1,m-1);
+        
+
+        
     }
 };
