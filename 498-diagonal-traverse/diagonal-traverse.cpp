6348c4aa071cfd409bfad6a2e65ab3286344aa2d
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-08-25 15:57:17 +0530

    Time: 0 ms (100.00%) | Memory: 22.4 MB (91.64%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -3,24 +3,40 @@ public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        vector<vector<int>> temp(n + m + 1);
         vector<int> ans;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                temp[i + j].push_back(mat[i][j]);
+        ans.reserve(n * m); // optional, avoids repeated reallocations
+
+        int i = 0, j = 0;
+        bool goingUp = true;
+
+        while (ans.size() < n * m) {
+            ans.push_back(mat[i][j]);
+
+            if (goingUp) {
+                if (j == m - 1) {      // hit last column
+                    i++; 
+                    goingUp = false;
+                } else if (i == 0) {   // hit first row
+                    j++;
+                    goingUp = false;
+                } else {               // move up-right
+                    i--;
+                    j++;
+                }
+            } else { // going down
+                if (i == n - 1) {      // hit last row
+                    j++;
+                    goingUp = true;
+                } else if (j == 0) {   // hit first column
+                    i++;
+                    goingUp = true;
+                } else {               // move down-left
+                    i++;
+                    j--;
+                }
             }
         }
-        for (int i = 0; i <= n + m; i++) {
-            if (i % 2 == 0) {
-                for (int j = temp[i].size() - 1; j >= 0; j--) {
-                    ans.push_back(temp[i][j]);
-                }
-            } else {
-                for (int j = 0; j < temp[i].size(); j++) {
-                    ans.push_back(temp[i][j]);
-                }
-            }
-        }
+
         return ans;
     }
 };
