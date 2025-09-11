8f6dfd1c7200eff04863eaf9581245d726381a6a
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-09-11 13:00:51 +0530

    Time: 43 ms (5.22%) | Memory: 15.6 MB (92.02%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,49 +1,26 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        vector<int>cap(26,0);
-        vector<int>small(26,0);
-        int n=s.size();
-        unordered_set<char>s1={'A','E','I','O','U'};
-        unordered_set<char>s2={'a','e','i','o','u'};
-        string ans="";
-        for(int i=0;i<n;i++){
-            ans+='#';
-        }
-        for(int i=0;i<n;i++){
-            if(s1.find(s[i])==s1.end()&&s2.find(s[i])==s2.end()){
-                ans[i]=s[i];
-            }
-            else if(s1.find(s[i])==s1.end()){
-                small[s[i]-'a']++;
+        auto isVowel = [](char c) {
+            return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+                   c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
+        };
 
-            }
-            else if(s2.find(s[i])==s2.end()){
-                cap[s[i]-'A']++;
-
-            }
-        }
-        string temp="";
-        for(int i=0;i<26;i++){
-            int m=cap[i];
-            for(int j=0;j<m;j++){
-                temp+=('A'+i);
-            }
-        }
-        for(int i=0;i<26;i++){
-            int m=small[i];
-            for(int j=0;j<m;j++){
-                temp+=('a'+i);
-            }
-        }
-        int ind=0;
-        for(int i=0;i<n;i++){
-            if(ans[i]=='#'){
-                ans[i]=temp[ind];
-                ind++;
-            }
-        }
-        return ans;
+        priority_queue<char, vector<char>, greater<char>> pq;
         
+        // Step 1: Push all vowels into min-heap
+        for (char c : s) {
+            if (isVowel(c)) pq.push(c);
+        }
+
+        // Step 2: Replace vowels in sorted order
+        for (char &c : s) {
+            if (isVowel(c)) {
+                c = pq.top();
+                pq.pop();
+            }
+        }
+
+        return s;
     }
 };
