8c6db5dec8e8ee9c39130f69a6d8a6722b5de178
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-09-21 22:45:29 +0530

    Time: 25 ms (77.34%) | Memory: 17.5 MB (67.49%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,1);//len of lis ending at index i
-        vector<int>cnt(n,1);//count pf lis ending at index i
-        int len=1;
-        for(int curr=0;curr<n;curr++){
-            for(int prev=0;prev<curr;prev++){
-                if(nums[curr]>nums[prev]){
-                    if(dp[prev]+1==dp[curr]){
-                        cnt[curr]+=cnt[prev];
-                    }
-                    else if(dp[prev]+1>dp[curr]){
-                        cnt[curr]=cnt[prev];
-                        dp[curr]=1+dp[prev];
+        int n = nums.size();
+        vector<int> lis(n, 1);
+        vector<int> temp(n, 1);
+        // length of lis ending at ith index
+        int ans = 0;
+        for (int curr = 0; curr < n; curr++) {
+            for (int prev = 0; prev < curr; prev++) {
+                if (nums[curr] > nums[prev]) {
+                    if (lis[curr] == 1+lis[prev]) {
+                        temp[curr] += temp[prev];
+                    } else if (lis[curr]<1+lis[prev]) {
+                        lis[curr]=1+lis[prev];
+                        temp[curr] = temp[prev];
                     }
                 }
             }
-            if(dp[curr]>len){
-                len=dp[curr];
+            ans=max(ans,lis[curr]);
+        }
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (lis[i] == ans) {
+                cnt += temp[i];
             }
         }
-        int res=0;
-        for(int i=0;i<n;i++){
-            if(dp[i]==len){
-                res+=cnt[i];
-            }
-        }
-        return res;
-        
+        return cnt;
     }
 };
