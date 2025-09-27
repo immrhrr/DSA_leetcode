5f84f5aa04f97608b353b2a5d50d6a7ff1dba3f2
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2025-09-27 23:53:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (9.16%) - LeetSync

---
+++ 830-largest-triangle-area/largest-triangle-area.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        int n = points.size();
+        double maxArea = 0.0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                for (int k = j + 1; k < n; ++k) {
+                    // use long long to avoid intermediate overflow, though ints are small here
+                    long long x1 = points[i][0], y1 = points[i][1];
+                    long long x2 = points[j][0], y2 = points[j][1];
+                    long long x3 = points[k][0], y3 = points[k][1];
+                    
+                    long long cross = x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2);
+                    double area = fabs((double)cross) / 2.0;
+                    if (area > maxArea) maxArea = area;
+                }
+            }
+        }
+        return maxArea;
+    }
+};
