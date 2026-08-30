aad8a608d4f41bea76316648a14dce62f52ecf1a
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2026-08-30 15:45:40 +0530

    Time: 0 ms (100.00%) | Memory: 92.3 MB (4.89%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        int n = nums.size();
-        if (n <= 2) return n;
-
-        int mini = INT_MAX, maxi = INT_MIN;
-        for (int i = 0; i < n; i++) {
-            mini = min(mini, nums[i]);
-            maxi = max(maxi, nums[i]);
+        int n=nums.size();
+        if(n==1||n==2)return n;
+        int mini=INT_MAX;
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            mini=min(mini,nums[i]);
+            maxi=max(maxi,nums[i]);
         }
-
-        int min_ind = -1, max_ind = -1;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == maxi) max_ind = i;
-            if (nums[i] == mini) min_ind = i;
-        } 
+        int min_ind=-1;
+        int max_ind=-1;
+        for(int i=0;i<n;i++){
+                if(nums[i]==maxi){
+                    max_ind=i;
+                }
+                if(nums[i]==mini){
+                    min_ind=i;
+                }
+            } 
         
-        int left = min(min_ind, max_ind);
-        int right = max(min_ind, max_ind);
+        int left=min(min_ind,max_ind);
+        int right=max(min_ind,max_ind);
+        //if they are at consecutive place then either form left or right side is possible
+        
+        int t1=right+1;
+        int t2=n-left;
+        int t3=left+1+n-right;
+        return min({t1,t2,t3});
+        
 
-        // 3 valid choices:
-        int delete_both_from_front = right + 1;
-        int delete_both_from_back  = n - left;
-        int delete_from_both_sides = (left + 1) + (n - right);
-
-        return min({delete_both_from_front, delete_both_from_back, delete_from_both_sides});
+        
     }
 };
