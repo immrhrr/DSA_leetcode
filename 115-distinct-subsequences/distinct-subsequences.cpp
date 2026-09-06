4436010474f4a5aa0e0a334db8dfcdf6d64772ff
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2026-09-06 15:23:10 +0530

    Time: 158 ms (8.54%) | Memory: 383.9 MB (8.25%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int solve(string&s,string&t,int i,int j){
-        if(j<0)return 1;
-        if(i<0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int take=0;
-        int notake=0;
-        if(s[i]==t[j]){
-            take=solve(s,t,i-1,j-1);
-        }
-        notake=solve(s,t,i-1,j);
-        return dp[i][j]= take+notake;
+    int solve(string &s, string &t,int i,int j,vector<vector<int>>&dp){
         
+        if(j==t.size())return 1;
+        if(i==s.size())return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+
+        if(s[i]==t[j]){
+            return dp[i][j]= solve(s,t,i+1,j,dp)+solve(s,t,i+1,j+1,dp);
+        }
+        else{
+            return dp[i][j]= solve(s,t,i+1,j,dp);
+        }
     }
     int numDistinct(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        dp.resize(n+1,vector<int>(m+1,-1));
-        return solve(s,t,n-1,m-1);
-        
-
-        
+        vector<vector<int>>dp(1001,vector<int>(1001,-1));
+        return solve(s,t,0,0,dp);
     }
 };
