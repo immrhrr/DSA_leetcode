0daf9884c946e089321b4d8b5a34ebff41d9d781
Hrithik Roshan <121360909+immrhrr@users.noreply.github.com>
2026-09-16 00:07:50 +0530

    Time: 2279 ms (5.23%) | Memory: 409.3 MB (5.76%) - LeetSync

---
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool check(string &s,int i,int j){
+        while(j>=i){
+            if(s[i]!=s[j])return false;
+            i++;j--;
+        }
+        return true;
+    }
+    int solve(string &s,int k,int i,int j,vector<vector<int>>&dp){
+        int n=s.size();
+        if(i>=n||j>=n)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(check(s,i,j)){
+            int take=1+solve(s,k,j+1,j+k,dp);
+            int grow=solve(s,k,i,j+1,dp);
+            int slide=solve(s,k,i+1,j+1,dp);
+            return dp[i][j]= max({take,slide,grow});
+        }
+        else{
+            int grow=solve(s,k,i,j+1,dp);
+            int slide=solve(s,k,i+1,j+1,dp);
+            return dp[i][j]= max({slide,grow});
+        }
+    }
+    int maxPalindromes(string s, int k) {
+        if(k==1)return s.size();
+        vector<vector<int>>dp(2000,vector<int>(2000,-1));
+        return solve(s,k,0,k-1,dp);
+        
+    }
+};
